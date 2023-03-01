e24cba25ba42d6ee6fcc4b02190d6b3e755d763e
VPhung24 <9276753+VPhung24@users.noreply.github.com>
2023-03-01 01:02:16 +0000

    .json and lazy error handling

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,20 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, json, url_for, render_template
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
-
+    return render_template('error.html', error = '404')
+    
 @app.route('/<id>', methods=['GET'])
-def request_uri(id):
-    return id
+def get_uri(id):
+    if id.endswith('.json'):
+        response = app.response_class(
+            response=json.dumps({'id': id.replace('.json', '')}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
+    return render_template('error.html', error = '404')
